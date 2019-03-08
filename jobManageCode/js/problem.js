25347c3210eb2656bae635d3e137298b72ab232f
suiwanling <suiwanlingfly@126.com>
2019-03-08 11:33:07 +0800

    Add files via upload

--- jobManageCode/js/problem.js
+++ jobManageCode/js/problem.js
@@ -1,6 +1,6 @@
 var problems = {
     info: [],
-    processState:function(problem){
+    processState: function (problem) {
         problem.state = problem.state[0];
         return this;
     },
@@ -37,7 +37,7 @@ var problems = {
                         title: '严重程度'
                     },
                 ],
-                data:problems.info,
+                data: problems.info,
             });
             return this;
         }
@@ -46,34 +46,77 @@ var problems = {
 
 var processers = {
     info: {},
-    static: [],
     getProcesserAndProblem: function (problem) {
         var __info = this.info;
-        if (!__info[problem.processer]) {
-            __info[problem.processer] = {
-                all:[],
-                processing:[],
-                finshed:[],
-                other:[]
+        var person = problem.processer;
+        if (!__info[person]) {
+            __info[person] = {
+                all: [],
+                state: {
+                    processing: [],
+                    finshed: [],
+                    other: []
+                },
+                grade: {
+                    little: [],
+                    normal: [],
+                    bad: [],
+                    terrible: []
+                }
             };
         }
         __info[problem.processer].all.push(problem);
-        return this;
-    },
-    getStatic: function () {
-        var info = this.info;
-        for (person in info) {
-            var staticInfo = {
-                processer: person,
-                problems: info[person].all.length,
-            }
-            this.static.push(staticInfo);
+
+        //按处理阶段统计 
+        if (('3' == problem.state) || ('5' == problem.state) || ('B' == problem.state)) {
+            __info[person].state.processing.push(problem);
+        } else if (('0' == problem.state) || ('E' == problem.state) || ('F' == problem.state) || ('G' == problem.state)) {
+            __info[person].state.finshed.push(problem);
+        } else {
+            __info[person].state.other.push(problem);
         }
-        this.static.sort(function (a, b) {
-            return (b.problems - a.problems);
-        });
+
+        //按问题级别统计
+        switch (problem.state) {
+            case '提示': {
+                __info[person].grade.little.push(problem);
+                break;
+            };
+            case '一般': {
+                __info[person].grade.normal.push(problem);
+                break;
+            };
+            case '严重': {
+                __info[person].grade.bad.push(problem);
+                break;
+            };
+            case '致命': {
+                __info[person].grade.terrible.push(problem);
+                break;
+            };
+            default: {
+                break;
+            }
+        }
         return this;
     },
+    // getStatic: function () {
+    //     var info = this.info;
+    //     for (person in info) {
+    //         var staticInfo = {
+    //             processer: person,
+    //             problems: info[person].all.length,
+    //             state:{
+
+    //             }
+    //         }
+    //         this.static.push(staticInfo);
+    //     }
+    //     this.static.sort(function (a, b) {
+    //         return (b.problems - a.problems);
+    //     });
+    //     return this;
+    // },
     bar: {
         id: echarts.init(document.getElementById('proceserEchart')),
         option: {
@@ -83,7 +126,10 @@ var processers = {
                 top: '50px',
                 bottom: '30px'
             },
-            color: ['#00B8D3'],
+            color: ['#00B8D3', '#aeea00', '#00bfa5'],
+            legend: {
+                data:['修改中','已完成','其他']
+            },
             xAxis: {
                 type: 'category',
                 data: [],
@@ -91,28 +137,55 @@ var processers = {
             yAxis: {
                 type: 'value'
             },
-            series: [{
-                type: 'bar',
-                data: [],
-            }]
+            series: [
+                {
+                    name: '修改中',
+                    stack: '问题',
+                    type: 'bar',
+                    data: [],
+                },
+                {
+                    name: '已完成',
+                    stack: '问题',
+                    type: 'bar',
+                    data: [],
+                },
+                {
+                    name: '其他',
+                    stack: '问题',
+                    type: 'bar',
+                    data: [],
+                },
+            ]
         },
         getData: function () {
-            var info = processers.static;
+            var info = processers.info;
             var data = {
                 person: [],
-                problemNum: []
+                problem: [],
+                state: {
+                    processing: [],
+                    finshed: [],
+                    other: []
+                }
             }
-            info.forEach(static => {
-                data.person.push(static.processer);
-                data.problemNum.push(static.problems);
-            })
+            for (person in info) {
+                var problem = info[person]
+                data.person.push(person);
+                data.problem.push(problem.all.length);
+                data.state.processing.push(problem.state.processing.length);
+                data.state.finshed.push(problem.state.finshed.length);
+                data.state.other.push(problem.state.other.length);
+            }
             return data;
         },
         setOption: function () {
             var data = this.getData();
             this.option.xAxis.data = data.person;
-            this.option.series[0].data = data.problemNum;
-
+            // this.option.series[0].data = data.problemNum;
+            this.option.series[0].data = data.state.processing;
+            this.option.series[1].data = data.state.finshed;
+            this.option.series[2].data = data.state.other;
             this.id.setOption(this.option);
         }
     }
@@ -144,7 +217,6 @@ function processData() {
         problems.processState(problem);
         processers.getProcesserAndProblem(problem);
     });
-    processers.getStatic();
 }
 
 $('#problem-file').change(function (e) {
